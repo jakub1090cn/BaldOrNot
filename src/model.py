3293f5124e2ae31234fe22a54af112bc557cb0fe
raodz <raodziem@gmail.com>
2024-07-31 13:08:21 +0200

    feat: constants in model

--- src/model.py
+++ src/model.py
@@ -1,19 +1,21 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications import ConvNeXtTiny
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
+from constants import IMG_LEN, NUM_CHANNELS, NUM_DENSE_UNITS
 
 
 def create_model():
     base_model = ConvNeXtTiny(
-        weights="imagenet", include_top=False, input_shape=(224, 224, 3)
+        weights="imagenet",
+        include_top=False,
+        input_shape=(IMG_LEN, IMG_LEN, NUM_CHANNELS),
     )
     for layer in base_model.layers:
         layer.trainable = False
-    inputs = Input(shape=(224, 224, 3))
+    inputs = Input(shape=(IMG_LEN, IMG_LEN, NUM_CHANNELS))
     x = base_model(inputs)
     x = GlobalAveragePooling2D()(x)
-    x = Dense(512, activation="relu")(x)
-    # 512 is only the initial number of units - it may be changed later
+    x = Dense(NUM_DENSE_UNITS, activation="relu")(x)
     predictions = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     return model
