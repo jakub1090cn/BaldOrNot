d3d087a2ba118e36d185ed98d0e9d0fb0f8711c2
raodz <raodziem@gmail.com>
2024-08-01 15:26:41 +0200

    chore: move variable num_dense_units

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
 from constants import IMG_LEN, NUM_CHANNELS
 
 
-def create_model():
+def create_model(num_dense_units=512):
     base_model = ConvNeXtTiny(
         weights="imagenet",
         include_top=False,
@@ -15,7 +15,6 @@ def create_model():
     inputs = Input(shape=(IMG_LEN, IMG_LEN, NUM_CHANNELS))
     x = base_model(inputs)
     x = GlobalAveragePooling2D()(x)
-    num_dense_units = 512
     x = Dense(num_dense_units, activation="relu")(x)
     predictions = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=inputs, outputs=predictions)
