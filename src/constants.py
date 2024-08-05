aa7bf4d46b3e15fb771792e633edb597889b25fc
jakub1090cn <jakubcymerman505@gmail.com>
2024-08-05 22:51:40 +0200

    chore: changed some constants

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
 import os
 
-DATA_PATH = os.path.join('..', 'src', 'data', 'list_attr_celeba.csv')
-DATASET_PATH = os.path.join('..', 'src', 'data', 'list_eval_partition.csv')
-IMAGES_PATH = os.path.join('..', 'src', 'data', 'img_align_celeba', 'img_align_celeba')
+DATA_PATH = os.path.join("..", "src", "data", "list_attr_celeba.csv")
+DATASET_PATH = os.path.join("..", "src", "data", "list_eval_partition.csv")
+IMAGES_PATH = os.path.join(
+    "..", "src", "data", "img_align_celeba", "img_align_celeba"
+)
