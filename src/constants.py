71a9c6a921f806f17e4d5acf79e71990c87ae2a0
jakub1090cn <jakubcymerman505@gmail.com>
2024-08-05 22:26:35 +0200

    chore: add path constants

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+import os
+
+DATA_PATH = os.path.join('..', 'src', 'data', 'list_attr_celeba.csv')
+DATASET_PATH = os.path.join('..', 'src', 'data', 'list_eval_partition.csv')
+IMAGES_PATH = os.path.join('..', 'src', 'data', 'img_align_celeba', 'img_align_celeba')
