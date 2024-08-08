880f1555d79143c2df5254dea16134f60cf51dba
jakub1090cn <jakubcymerman505@gmail.com>
2024-08-08 21:36:16 +0200

    chore: moved counting correct images

--- src/data.py
+++ src/data.py
@@ -16,10 +16,10 @@ def check_images(directory: str) -> Tuple[List[str], int, int]:
 
             if img is None or img.size == 0:
                 empty_or_corrupted.append(filename)
-            else:
-                num_correct += 1
         except Exception:
             empty_or_corrupted.append(filename)
+        else:
+            num_correct += 1
 
     return empty_or_corrupted, len(empty_or_corrupted), num_correct
 
