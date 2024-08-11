53fbbf40fa2a9dd0bf111b6fc25d7599a3c0d642
jakub1090cn <jakubcymerman505@gmail.com>
2024-08-11 20:19:03 +0200

    chore: move data paths to config

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,3 @@
-import os
-
-DATA_PATH = os.path.join("..", "src", "data", "list_attr_celeba.csv")
-DATASET_PATH = os.path.join("..", "src", "data", "list_eval_partition.csv")
-IMAGES_PATH = os.path.join(
-    "..", "src", "data", "img_align_celeba", "img_align_celeba"
-)
 BALD_LABELS = {-1: "Not Bald", 1: "Bald"}
-
 IMG_LEN = 224
 NUM_CHANNELS = 3
