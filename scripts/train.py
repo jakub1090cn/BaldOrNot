564e1d62a8bb2fc5ddcc510e5eea91598b173bc5
tomaszs <skrzypczykt@gmail.com>
2024-08-20 18:26:18 +0200

    as package

--- scripts/train.py
+++ scripts/train.py
@@ -6,13 +6,3 @@ from src.model_training import run_experiment
 if __name__ == "__main__":
     config = CLI(BoldOrNotConfig)
     run_experiment(config=config)
-
-## --------- colab
-# import yaml
-# from src.config import BoldOrNotConfig
-# from src.model_training import run_experiment
-#
-# CONFIG_PATH = "path/to/my/gdrive/config.yaml"
-# with open(CONFIG_PATH, "r") as fp:
-#     config = BoldOrNotConfig(yaml.safe_load(fp))
-# run_experiment(config=config)
